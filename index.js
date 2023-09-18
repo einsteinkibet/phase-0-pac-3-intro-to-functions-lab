fda0cc326c525593d0ef9af7474c6aa124c9e0fd
einsteinkibet <einsteinkibet0110@gmail.com>
2023-09-18 14:12:23 +0300

    Comleted assignment

---
+++ index.js
@@ -0,0 +1,54 @@
+function shout(string){
+    return string.toUpperCase();
+}
+console.log(shout("hello"))
+
+
+
+
+function whisper(string){
+    return string.toLowerCase();
+}
+console.log(whisper("HELLO"));
+
+
+
+
+function logShout(string) {
+    console.log(string.toUpperCase());
+  }
+  logShout('hello');
+  console.logRestore();
+
+
+
+  function logWhisper(string) {
+    console.log(string.toLowerCase());
+  }
+  logWhisper('hello');
+  console.logRestore();
+
+
+
+  function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+      return "I can't hear you!";
+    } else if (string === string.toUpperCase()) {
+      return "YES INDEED!";
+    }
+  }
+
+
+
+  function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+      return "I can't hear you!";
+    } else if (string === string.toUpperCase()) {
+      return "YES INDEED!";
+    } else if (string === "Let's have dinner together!") {
+      return "I would love to!";
+    }
+}
+  
+
+  
